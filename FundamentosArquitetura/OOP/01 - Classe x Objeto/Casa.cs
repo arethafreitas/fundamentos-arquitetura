fb3baf18c3db7b23f8f47a87b80b70e95c611c78
Aretha Lemos de Freitas <aretha.lemos@icloud.com>
2022-12-10 17:14:00 -0300

    add: inclusão do projeto + conceito de Estado x Comportamento

---
+++ FundamentosArquitetura/OOP/01 - Classe x Objeto/Casa.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP._01___Classe_x_Objeto
+{
+    public class Casa
+    {
+        //Definição de Classe: Mapeia um objeto do mundo real, possuindo propriedades
+        public int TamanhoM2 { get; set; }
+        public int Andares { get; set; }
+        public decimal Valor { get; set; }
+        public int NumeroVagas { get; set; }
+    }
+
+    public class Objeto
+    {
+        //Definição de Objeto: Classe sendo instanciada, representada na memória como um Objeto. Possui valores.
+        public Objeto()
+        {
+            _ = new Casa
+            {
+                TamanhoM2 = 100,
+                Andares = 2,
+                Valor = 100000,
+                NumeroVagas = 3
+            };
+        }
+    }
+}
---
+++ FundamentosArquitetura/OOP/02 - Pilares OOP/01 - EstadoComportamento.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP._02___Pilares_OOP
+{
+    public class Pessoa
+    {
+        //Estado: Propriedades da classe. Precisam estar em um Estado, armazenando informações,
+        //possuindo um estado definido
+        public string Nome { get; set;}
+        public DateTime DataNascimento { get; set;}
+
+        //Comportamento: Métodos que geram informação e devolvem outras.
+        //Processamento, transformação das informações. Modificam valores
+        public int CalcularIdade()
+        {
+            var dataAtual = DateTime.Now;
+            var idade = dataAtual.Year - DataNascimento.Year;
+
+            if (dataAtual < DataNascimento.AddYears(idade)) idade--;
+
+            return idade;
+        }
+    }
+}
